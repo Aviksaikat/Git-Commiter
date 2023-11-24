2772d49b744531c2825855445cb4de5cda9cb517
SAIKAT KARMAKAR <saikickkarma@gmail.com>
2023-11-24 01:57:00 +0000

    feat: Update and modify files for the project

--- gcm.sh
+++ gcm.sh
@@ -1,18 +1,40 @@
 #!/bin/bash
 
+# Check if directory argument is provided
+if [ -z "$1" ]; then
+  echo "Usage: $0 <directory>"
+  exit 1
+fi
+
+# Move to the specified directory
+cd "$1" || exit 1
+
 # Add modified files
 git diff --name-only --cached > modified_files.txt
 
 # Add untracked files
 git ls-files --others --exclude-standard >> modified_files.txt
 
-# Create commit message
-commit_message="feat: Add and modify files for the project
+# Separate added and modified files
+added_files=""
+modified_files=""
 
-$(awk '{print "- " $1}' modified_files.txt)"
+while IFS= read -r file; do
+  if [ -e "$file" ]; then
+    modified_files+="\n- Update ${file}"
+  else
+    added_files+="\n- Add new file: ${file}"
+  fi
+done < modified_files.txt
 
-# Add the files
+# Stage all changes
 git add .
+
+# Create commit messages
+commit_message="feat: Update and modify files for the project"
+[ -n "$modified_files" ] && commit_message+=" (modified files:${modified_files})"
+[ -n "$added_files" ] && commit_message+=" (added files:${added_files})"
+
 # Commit changes
 git commit -m "$commit_message"
 
